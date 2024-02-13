cd310031d48397f5d633dbf135153c576e124652
unknown <@ngknn.local>
2024-02-13 13:15:30 +0300

    first page + second page

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -26,20 +26,33 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.unit.dp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.imageResource
 
 @Preview
 @Composable
 fun SmartLab ()
 {
-    val myColor: Color = Color(0xFF0000FF)
+    val myColor: Color = Color(53252532)
 
     Column (
         modifier = Modifier.run {
             fillMaxSize()
-                .background(myColor)
+                .background(Color.Gray)
         }
     ) {
+        Image( 
 
+            painter = BitmapPainter(ImageBitmap.imageResource(R.drawable.logo)), modifier = Modifier.padding(top = 375.dp,start = 95.dp).width(200.dp).height(50.dp),
+            contentDescription = "Смарт лаб"
+        )
 
     }
 }
@@ -55,17 +68,17 @@ fun LogIn () {
         }
     ) {
         Text(
-            text = "✋ Добро пожаловать!",fontFamily=FontFamily.SansSerif,fontSize = 20.sp,
+            text = "\n\n\n✋ Добро пожаловать!",fontFamily=FontFamily.SansSerif,fontSize = 20.sp,
             fontWeight = FontWeight.Bold,textAlign = TextAlign.Center,modifier = Modifier.fillMaxWidth(1f)
         )
         Text(
             text = " Войдите, чтобы воспользоваться \n функциями приложения", fontSize = 16.sp,fontFamily=FontFamily.SansSerif
         )
         Text(
-            text = "Вход по E-mail", color = Color.Gray,fontStyle = FontStyle.Italic,fontSize = 12.sp,fontFamily=FontFamily.SansSerif
+            text = "\n\n\n\n\n\n\n\nВход по E-mail", color = Color.Gray,fontStyle = FontStyle.Italic,fontSize = 12.sp,fontFamily=FontFamily.SansSerif
         )
         TextField(
-            value = "example@mail.ru", onValueChange = {"0_0"}
+            value  = "example@mail.ru", onValueChange = {"0_0"}
         )
         val label = remember{mutableStateOf("Click")}
         Button(onClick = {label.value = "Проверяем введенные данные"}) {
@@ -75,10 +88,10 @@ fun LogIn () {
         }
 
         Text(
-            text = "Или войти с помощью", color = Color.Gray,fontStyle = FontStyle.Italic,fontSize = 12.sp,fontFamily=FontFamily.SansSerif,textAlign = TextAlign.Center,modifier = Modifier.fillMaxWidth(1f)
+            text = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nИли войти с помощью", color = Color.Gray,fontStyle = FontStyle.Italic,fontSize = 12.sp,fontFamily=FontFamily.SansSerif,textAlign = TextAlign.Center,modifier = Modifier.fillMaxWidth(1f)
         )
         Button(onClick = {}, modifier = Modifier
-            .padding(top = 550.dp)) {
+            .padding()) {
             Text(text = "Войти с Яндекс",fontFamily=FontFamily.SansSerif,fontSize = 14.sp,textAlign = TextAlign.Center)
         }
 
